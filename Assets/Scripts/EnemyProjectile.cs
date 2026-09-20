4e4daac3afde6bf43f5a704744faeca9c77e5d92
Sid <austinsidney52897@gmail.com>
2026-09-20 00:51:47 -0500

    Added Heavy Enemy
    - Stationary Heavy Enemy with reusable firing script.
    - Projectile damage
    - Projectile lifetime cleanup

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+
+    [SerializeField] float speed = 6f;
+    [SerializeField] int damage = 2;
+    [SerializeField] float lifetime = 4f;
+
+    Vector2 direction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    public void SetDirection(Vector2 newDirection)
+    {
+        direction = newDirection.normalized;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3)(direction * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IDamage damageable = collision.GetComponent<IDamage>();
+
+        if (damageable != null)
+        {
+            damageable.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
